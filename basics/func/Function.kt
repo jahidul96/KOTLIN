ef2bfb61cf3c810220ef47a340cd7e2f357de96e
jahidul96 <jahidul59895@gmail.com>
2022-12-07 13:41:19 +0600

    function added

--- basics/func/Function.kt
+++ basics/func/Function.kt
@@ -1,14 +1,25 @@
 
+ fun num(a: Int, b: Int) : Int{
+    return a + b;
+ }
 
-fun main() {
-    
 
-    fun num() : Int{
-       return 1234;
+ fun isPrime( a : Int) : Boolean{
+    if(a <= 1){
+        return false
     }
 
+    var i = 2;
+    while(i < a){
+        if(a%i==0){
+            return false
+        }
+        ++i
+    }
+    return true
+ }
 
-    val res: Int = num();
-
-    println("this is num result $res")
+fun main() {
+  
+ 
 }
